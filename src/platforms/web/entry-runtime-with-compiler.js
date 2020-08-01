2867985d3b4d43cc6c68d94a6bfeeda14f33e6c3
yamei94 <1016112504@qq.com>
2020-08-01 10:36:32 +0800

    初始化vue实例同时传入template和render函数会执行谁

--- src/platforms/web/entry-runtime-with-compiler.js
+++ src/platforms/web/entry-runtime-with-compiler.js
@@ -1,101 +1,119 @@
 /* @flow */
 
-import config from 'core/config'
-import { warn, cached } from 'core/util/index'
-import { mark, measure } from 'core/util/perf'
+import config from "core/config";
+import { warn, cached } from "core/util/index";
+import { mark, measure } from "core/util/perf";
 
-import Vue from './runtime/index'
-import { query } from './util/index'
-import { compileToFunctions } from './compiler/index'
-import { shouldDecodeNewlines, shouldDecodeNewlinesForHref } from './util/compat'
+import Vue from "./runtime/index";
+import { query } from "./util/index";
+import { compileToFunctions } from "./compiler/index";
+import {
+  shouldDecodeNewlines,
+  shouldDecodeNewlinesForHref,
+} from "./util/compat";
 
-const idToTemplate = cached(id => {
-  const el = query(id)
-  return el && el.innerHTML
-})
+const idToTemplate = cached((id) => {
+  const el = query(id);
+  return el && el.innerHTML;
+});
 
-const mount = Vue.prototype.$mount
+// 保留vue实例的$mount方法
+const mount = Vue.prototype.$mount;
+// $mount：把DOM挂载到页面上
 Vue.prototype.$mount = function (
   el?: string | Element,
+  // 非str的情况下是false，str的时候是true
   hydrating?: boolean
 ): Component {
-  el = el && query(el)
+  // 获取el对应的DOM对象
+  el = el && query(el);
 
   /* istanbul ignore if */
+  // el不能是html或者body，vue实例只能挂载在普通的dom元素上
   if (el === document.body || el === document.documentElement) {
-    process.env.NODE_ENV !== 'production' && warn(
-      `Do not mount Vue to <html> or <body> - mount to normal elements instead.`
-    )
-    return this
+    // 如果是开发模式，会在浏览器控制台报出警告
+    process.env.NODE_ENV !== "production" &&
+      warn(
+        `Do not mount Vue to <html> or <body> - mount to normal elements instead.`
+      );
+    // 如果是生产模式直接返回当前vue实例
+    return this;
   }
 
-  const options = this.$options
+  const options = this.$options;
   // resolve template/el and convert to render function
+  // 把template转换成render函数
   if (!options.render) {
-    let template = options.template
+    let template = options.template;
     if (template) {
-      if (typeof template === 'string') {
-        if (template.charAt(0) === '#') {
-          template = idToTemplate(template)
+      if (typeof template === "string") {
+        if (template.charAt(0) === "#") {
+          template = idToTemplate(template);
           /* istanbul ignore if */
-          if (process.env.NODE_ENV !== 'production' && !template) {
+          if (process.env.NODE_ENV !== "production" && !template) {
             warn(
               `Template element not found or is empty: ${options.template}`,
               this
-            )
+            );
           }
         }
       } else if (template.nodeType) {
-        template = template.innerHTML
+        template = template.innerHTML;
       } else {
-        if (process.env.NODE_ENV !== 'production') {
-          warn('invalid template option:' + template, this)
+        if (process.env.NODE_ENV !== "production") {
+          warn("invalid template option:" + template, this);
         }
-        return this
+        return this;
       }
     } else if (el) {
-      template = getOuterHTML(el)
+      template = getOuterHTML(el);
     }
     if (template) {
       /* istanbul ignore if */
-      if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
-        mark('compile')
+      if (process.env.NODE_ENV !== "production" && config.performance && mark) {
+        mark("compile");
       }
 
-      const { render, staticRenderFns } = compileToFunctions(template, {
-        outputSourceRange: process.env.NODE_ENV !== 'production',
-        shouldDecodeNewlines,
-        shouldDecodeNewlinesForHref,
-        delimiters: options.delimiters,
-        comments: options.comments
-      }, this)
-      options.render = render
-      options.staticRenderFns = staticRenderFns
+      const { render, staticRenderFns } = compileToFunctions(
+        template,
+        {
+          outputSourceRange: process.env.NODE_ENV !== "production",
+          shouldDecodeNewlines,
+          shouldDecodeNewlinesForHref,
+          delimiters: options.delimiters,
+          comments: options.comments,
+        },
+        this
+      );
+      options.render = render;
+      options.staticRenderFns = staticRenderFns;
 
       /* istanbul ignore if */
-      if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
-        mark('compile end')
-        measure(`vue ${this._name} compile`, 'compile', 'compile end')
+      if (process.env.NODE_ENV !== "production" && config.performance && mark) {
+        mark("compile end");
+        measure(`vue ${this._name} compile`, "compile", "compile end");
       }
     }
   }
-  return mount.call(this, el, hydrating)
-}
+
+  // 如果传递了render函数会调用mount方法渲染DOM
+  return mount.call(this, el, hydrating);
+};
 
 /**
  * Get outerHTML of elements, taking care
  * of SVG elements in IE as well.
  */
-function getOuterHTML (el: Element): string {
+function getOuterHTML(el: Element): string {
   if (el.outerHTML) {
-    return el.outerHTML
+    return el.outerHTML;
   } else {
-    const container = document.createElement('div')
-    container.appendChild(el.cloneNode(true))
-    return container.innerHTML
+    const container = document.createElement("div");
+    container.appendChild(el.cloneNode(true));
+    return container.innerHTML;
   }
 }
 
-Vue.compile = compileToFunctions
+Vue.compile = compileToFunctions;
 
-export default Vue
+export default Vue;
--- src/platforms/web/util/index.js
+++ src/platforms/web/util/index.js
@@ -1,25 +1,29 @@
 /* @flow */
 
-import { warn } from 'core/util/index'
+import { warn } from "core/util/index";
 
-export * from './attrs'
-export * from './class'
-export * from './element'
+export * from "./attrs";
+export * from "./class";
+export * from "./element";
 
 /**
  * Query an element selector if it's not an element already.
  */
-export function query (el: string | Element): Element {
-  if (typeof el === 'string') {
-    const selected = document.querySelector(el)
+export function query(el: string | Element): Element {
+  // el如果是字符串类型，如果不是，就认为el是DOM对象，直接返回
+  // 如果是字符串，就认为el是选择器，通过document.querySelector获取选择器对应的DOM对象
+  if (typeof el === "string") {
+    const selected = document.querySelector(el);
     if (!selected) {
-      process.env.NODE_ENV !== 'production' && warn(
-        'Cannot find element: ' + el
-      )
-      return document.createElement('div')
+      // 如果没有找到el对应DOM对象，通过环境变量env去判断当前是开发模式还是生产模式
+      // 如果是开发模式，就在浏览器的控制台报出警告
+      process.env.NODE_ENV !== "production" &&
+        warn("Cannot find element: " + el);
+      // 如果是生产模式就返回一个div
+      return document.createElement("div");
     }
-    return selected
+    return selected;
   } else {
-    return el
+    return el;
   }
 }
